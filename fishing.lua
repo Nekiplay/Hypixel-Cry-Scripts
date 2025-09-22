d6e5f37dfdf6c0d3a343e7b0047c46749dcd5484
Roman Danilov <35975332+Nekiplay@users.noreply.github.com>
2025-09-23 00:40:48 +0500

    Add files via upload

--- fishing.lua
+++ fishing.lua
@@ -1,5 +1,8 @@
+local rotations = require("rotations_v2")
+
 -- Настройки задержки (в тиках)
 local PRESS_DELAY = 5   -- Задержка перед нажатием
+local ABILITY_DELAY = 18   -- Задержка перед нажатием
 local RELEASE_DELAY = 3 -- Задержка перед отжатием
 
 local state = {
@@ -16,7 +19,7 @@ registerClientTick(function()
     for index, entity in ipairs(entities) do
         if entity ~= nil then
             local entityName = entity.name
-            if string.find(entityName, "!") or string.find(entityName, "ǃ") or string.find(entityName, "ꜝ") and player.fishHook then
+            if string.find(entityName, "!!!") or string.find(entityName, "ǃǃǃ") or string.find(entityName, "ꜝꜝꜝ") and player.fishHook then
                 foundTarget = true
                 
                 if state.phase == "idle" then
@@ -43,10 +46,16 @@ registerClientTick(function()
     elseif state.phase == "pressed" then
         -- Удерживаем нажатие, пока есть цель
         if not foundTarget then
+            state.phase = "ability"
+            state.tick = 0
+        end
+    elseif state.phase == "ability" then
+        state.tick = state.tick + 1
+        if state.tick >= ABILITY_DELAY then
+            player.input.silentUse(1)
             state.phase = "releasing"
             state.tick = 0
         end
-    
     elseif state.phase == "releasing" then
         state.tick = state.tick + 1
         if state.tick >= RELEASE_DELAY then
@@ -58,7 +67,22 @@ registerClientTick(function()
         end
     
     elseif state.phase == "idle" then
-        -- Ничего не делаем, ждем новую цель
+        local entities = world.getEntities()
+        local foundTarget = false
+        
+        -- Поиск целевой entity
+        for index, entity in ipairs(entities) do
+            if entity ~= nil then
+                local entityName = entity.name
+                
+                local bx = px + entity.x
+                local by = py + entity.y
+                local bz = pz + entity.z
+
+                local distSq = entity.x*dx + dy*dy + dz*dz
+
+            end
+        end
 
     end
 end)
